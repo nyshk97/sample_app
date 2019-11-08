da56ecf99caa7600ec6db98b79579b138d5716d9
tsubasa namatame <nyshk97@gmail.com>
2019-11-08 09:37:33 +0900

    Until 10.4.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,8 +40,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
     end
 
     # ログイン済みユーザーかどうか確認
