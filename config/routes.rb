3d5b6147f8a8c8bca4df2edfd4b1144d0f215836
tsubasa namatame <nyshk97@gmail.com>
2019-11-16 14:45:08 +0900

    Complete 14.2.4

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,4 @@ Rails.application.routes.draw do
   resources :password_resets, only:[:new, :create, :edit, :update]
   resources :microposts, only:[:create, :destroy]
   resources :relationships, only:[:create, :destroy]
-  # resources :relationships, only:[:create, :destroy]
 end
